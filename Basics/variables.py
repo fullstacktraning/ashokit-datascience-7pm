5510015dd262e8067d542ce13144d0192aa899b8
AshokIT <info@ashokit.in>
2026-03-26 19:48:17 +0530

    variables & datatypes  started

---
+++ Basics/variables.py
@@ -0,0 +1,42 @@
+num1 = 200
+num2 = 100
+addition = num1 + num2
+print(addition)
+subtraction = num1 - num2
+print(subtraction)
+temp = 98.3
+print(temp)
+
+
+sub1 = "ML"
+sub2 = 'DL'
+sub3 = """
+        NLP (Natual Language Processing)
+        Gen AI (Generative AI)
+        Agentic AI
+"""
+print(sub1)
+print(sub2)
+print(sub3)
+name = "Python"
+print(name[0], name[-1])
+print(name[2],name[-4])
+print(name[0:3])
+print(name[-3:-1])
+print(name[:4])
+print(name[2:])
+print(name[::-1])
+#name[0] = "Q"
+name = "Q" + name[1:]
+print(name)
+msg1 = "Hello"
+msg2 = "Machine Learning !!!"
+print(msg1 + msg2)
+print(msg1 * 3)
+test = "hello"
+print(test.upper())
+test1 = "WELCOME"
+print(test1.lower())
+print(test1.capitalize())
+print(test1.find("L"))
+print(test1.index("L"))
