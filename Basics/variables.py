715dfd7200a69e00d871d991535ca23d78513515
AshokIT <info@ashokit.in>
2026-03-27 19:54:15 +0530

    code

--- Basics/variables.py
+++ Basics/variables.py
@@ -1,42 +1,103 @@
-num1 = 200
-num2 = 100
-addition = num1 + num2
-print(addition)
-subtraction = num1 - num2
-print(subtraction)
-temp = 98.3
-print(temp)
+# num1 = 200
+# num2 = 100
+# addition = num1 + num2
+# print(addition)
+# subtraction = num1 - num2
+# print(subtraction)
+# temp = 98.3
+# print(temp)
 
 
-sub1 = "ML"
-sub2 = 'DL'
-sub3 = """
-        NLP (Natual Language Processing)
-        Gen AI (Generative AI)
-        Agentic AI
-"""
-print(sub1)
-print(sub2)
-print(sub3)
-name = "Python"
-print(name[0], name[-1])
-print(name[2],name[-4])
-print(name[0:3])
-print(name[-3:-1])
-print(name[:4])
-print(name[2:])
-print(name[::-1])
-#name[0] = "Q"
-name = "Q" + name[1:]
-print(name)
-msg1 = "Hello"
-msg2 = "Machine Learning !!!"
-print(msg1 + msg2)
-print(msg1 * 3)
-test = "hello"
-print(test.upper())
-test1 = "WELCOME"
-print(test1.lower())
-print(test1.capitalize())
-print(test1.find("L"))
-print(test1.index("L"))
+# sub1 = "ML"
+# sub2 = 'DL'
+# sub3 = """
+#         NLP (Natual Language Processing)
+#         Gen AI (Generative AI)
+#         Agentic AI
+# """
+# print(sub1)
+# print(sub2)
+# print(sub3)
+# name = "Python"
+# print(name[0], name[-1])
+# print(name[2],name[-4])
+# print(name[0:3])
+# print(name[-3:-1])
+# print(name[:4])
+# print(name[2:])
+# print(name[::-1])
+# #name[0] = "Q"
+# name = "Q" + name[1:]
+# print(name)
+# msg1 = "Hello"
+# msg2 = "Machine Learning !!!"
+# print(msg1 + msg2)
+# print(msg1 * 3)
+# test = "hello"
+# print(test.upper())
+# test1 = "WELCOME"
+# print(test1.lower())
+# print(test1.capitalize())
+# print(test1.find("L"))
+# print(test1.index("L"))
+
+
+#27-mar-2026
+# str = "a,b,c"
+# print(str.split(",")) #['a', 'b', 'c']
+
+# str = "a,b,c"
+# print(str.replace("a","x")) #x,b,c
+
+# str = ["welcome","to","Gen AI"]
+# print(" ".join(str)) #welcome to Gen AI
+
+# str = " hello "
+# print(str.strip())      #hello
+# print(str.lstrip()) 
+# print(str.rstrip())
+
+# name = "Samba"
+# print("Hello {}".format(name))
+
+# name = "Samba"
+# print(f"Hello {name}")
+
+# str = "madam"
+# res = "Palindrome" if str == str[::-1] else "Not Palindrome"
+# print(res)
+
+# str = "banana"
+# print("".join(set(str)))
+
+
+# str1 = "listen"
+# str2 = "silent"
+# res = "Anagram" if sorted(str1) == sorted(str2) else "Not Anagram"
+# print(res)
+
+# print(ord("A")) #65
+# print(chr(65))  #A
+
+# for ch in "python":
+#     print(ch)
+
+# Boolean
+# True & False
+# True - 1 & False - 0
+# flag = True
+# flag1 = False
+# print(type(flag)) # <class 'bool'>
+# print(True + True) # 2
+# print(True * 5)   # 5
+# print(False + 10)  # 10
+# print(5 > 3) #True
+# print(5 < 3) #False
+# print(5 == 5) #True
+# print(5 != 5) #False
+# print(True and False) #False
+# print(True or False) #True
+# print(not True) #False
+
+print( 3>2>1 ) #True
+print( 1<2<3 ) #True
